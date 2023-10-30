c784762f6d62b18ec0946b8b332dc9c3d8864f8f
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <142774957+AnonymousX0408@users.noreply.github.com>
2023-10-30 18:12:40 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -63,7 +63,7 @@ async def help_message_f(client, message):
             return
         except Exception:
             await message.reply_text(
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Something went Wrong. Contact my [Support Group](https://t.me/Prime_Rolex).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -74,16 +74,14 @@ async def help_message_f(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
+                    InlineKeyboardButton('Updates Channel', url='https://t.me/Privates_RoBot')
                 ],
                 [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
+                    InlineKeyboardButton('Support Group', url='https://t.me/PrivatesRoBotChats')
                 ],
                 [
-                    InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
-                    InlineKeyboardButton('Source Code', url='https://github.com/AbirHasan2005/VideoCompress') # Must Give us Credits!
+                    InlineKeyboardButton('Developer', url='https://t.me/Prime_Rolex'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
                 ]
-            ]
         ),
         quote=True
     )
